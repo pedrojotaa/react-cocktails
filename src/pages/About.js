b45e87fed4a949d88daefbf9e89248bde4200aa9
“João <eng.joao.pedro@outlook.com>
2023-01-24 18:33:11 -0300

    refactor: structure about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
-  )
-}
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod
+        tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam,
+        nisi ut aliquid ex ea commodi consequatur. Quis aute iure reprehenderit
+        in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+        Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui
+        officia deserunt mollit anim id est laborum.
+      </p>
+    </section>
+  );
+};
 
-export default About
+export default About;
