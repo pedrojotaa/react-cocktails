245c6d476305b92a49ba443b908d9e526f3afa05
“João <eng.joao.pedro@outlook.com>
2023-01-25 18:43:18 -0300

    refactor: structure search of drinks

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,13 +1,38 @@
-import React from 'react'
-import { useGlobalContext } from '../context'
+import React from "react";
+import { useGlobalContext } from "../context";
 
 const SearchForm = () => {
-  const {setSearchTerm} = useGlobalContext()
-  return (
-    <div>
-      <h2>search form component</h2>
-    </div>
-  )
-}
+  const { setSearchTerm } = useGlobalContext();
 
-export default SearchForm
+  const searchValue = React.useRef("");
+
+  const searchCocktail = () => {
+    setSearchTerm(searchValue.current.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  React.useEffect(() => {
+    searchValue.current.focus();
+  });
+
+  return (
+    <section className="section search">
+      <form className="search-form" onSubmit={handleSubmit}>
+        <div className="form-control">
+          <label htmlFor="name">search your favorite cocktail</label>
+          <input
+            type="text"
+            id="name"
+            ref={searchValue}
+            onChange={searchCocktail}
+          />
+        </div>
+      </form>
+    </section>
+  );
+};
+
+export default SearchForm;
