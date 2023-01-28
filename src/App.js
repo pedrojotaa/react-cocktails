26fd64689a2eaae19c5bba8deab8f9aec4a5060d
“João <eng.joao.pedro@outlook.com>
2023-01-28 20:35:26 -0300

    refactor: fixing route error

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
-import Navbar from './components/Navbar'
-import Home from './pages/Home'
-import About from './pages/About'
-import SingleCocktail from './pages/SingleCocktail'
-import Error from './pages/Error'
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SingleCocktail from "./pages/SingleCocktail";
+import Error from "./pages/Error";
 
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <Router>
-      <Navbar/>
+      <Navbar />
       <Routes>
-        <Route exact path='/' element={<Home/>}/>
-        <Route exact path='/about' element={<About/>}/>
-        <Route exact path='/cocktail:id' element={<SingleCocktail/>}/>
-        <Route exact path='*' element={<Error/>}/>
+        <Route exact path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="cocktail/:id" element={<SingleCocktail />} />
+       {/*  <Route exact path="*" element={<Error />} /> */}
       </Routes>
     </Router>
   );
